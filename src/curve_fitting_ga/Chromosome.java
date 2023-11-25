d300dba6b7c4ff80779f68b92ca11ae1859fdf6a
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2023-11-25 21:38:19 +0200

    comments

--- src/curve_fitting_ga/Chromosome.java
+++ src/curve_fitting_ga/Chromosome.java
@@ -2,11 +2,12 @@ package curve_fitting_ga;
 
 public class Chromosome {
 	
-	double fitness = 0.0;
+	double fitness;
 	double[] coefficients;
 	
 	public Chromosome(int degree)
 	{
+		this.fitness = 0.0;
 		this.coefficients = new double [degree + 1];
 	}
 }
--- src/curve_fitting_ga/curveFittingGA.java
+++ src/curve_fitting_ga/curveFittingGA.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 
 public class CurveFittingGA {
 	
+	//initializing some variables
     private int polynomialDegree;
     private Point[] points;
     private int populationSize = 100;
@@ -12,13 +13,13 @@ public class CurveFittingGA {
     private Chromosome[] population;
     private Chromosome[] parents;
     private Chromosome[] children;
-    private int eliteCount =50;
+    private int eliteCount =30;
     private static int testcase = 0;
     private double upperBound = 10;
     private double lowerBound = -10;
     private double Pc = 0.7;
     private double Pm = 0.01;
-    private int b = 1;
+    private int b = 1;  // for nonunifrom mutation
     private int nonElite = populationSize - eliteCount;
     
     public CurveFittingGA(int polynomialDegree, Point[] points) {
@@ -29,23 +30,43 @@ public class CurveFittingGA {
         this.parents = new Chromosome[nonElite];
         this.children = new Chromosome[nonElite];
         
-        for (int i = 0; i < populationSize; i++)
-        {
+        
+        // Initializing population
+        for (int i = 0; i < populationSize; i++) {
             population[i] = new Chromosome(polynomialDegree);
-            if (i >= eliteCount)
-            {
-                parents[i - eliteCount] = new Chromosome(polynomialDegree);
-                children[i - eliteCount] = new Chromosome(polynomialDegree);
-            }
 
-            for (int j = 0; j < polynomialDegree + 1; j++)
-            {
+            for (int j = 0; j < polynomialDegree + 1; j++) {
                 population[i].coefficients[j] = (20 * Math.random()) - 10;
+                // round to 2 decimal points
                 population[i].coefficients[j] = Math.round(100 * population[i].coefficients[j]) / 100.0;
             }
         }
+
+        // Initializing parents and children
+        for (int i = eliteCount; i < populationSize; i++) {
+            parents[i - eliteCount] = new Chromosome(polynomialDegree);
+            children[i - eliteCount] = new Chromosome(polynomialDegree);
+        }
+
     }
     
+    public void writeToFile() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("results.txt", true))) {
+            writer.println("Dataset: " + testcase);
+            for (int i = 0; i <= polynomialDegree; i++) {
+                writer.println("Coefficient " + i + " = " + population[0].coefficients[i]);
+            }
+            writer.println("Mean Square Error = " + population[0].fitness);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    /**
+     * Call functions by order
+     * Fitness  - > Sort - > selection - > crossover - > mutation -> replacement
+     */
     public void geneticAlgorithm() {
         for (int i = 0; i < generations; i++) {
             mseFitness();
@@ -54,18 +75,28 @@ public class CurveFittingGA {
             twoPointCrossover();
             nonUniformMutation();
             elitistReplacement();
-        }
+        }        
+        solveBestSolution();
+    }
+    
+    
+    //To get best solution in population and write it into file
+    
+    public void solveBestSolution()
+    {
         mseFitness();
         sortPopulation(population);
         writeToFile();
     }
 
+    //sort the population according to fitness
     public void sortPopulation(Chromosome[] population) {
         Arrays.sort(population, Comparator.comparingDouble(chromosome -> chromosome.fitness));
     }
 
     public void mseFitness()
     {
+    	//Perform mse equation on each chromosome
         double total = 0.0;
 
         for (int i = 0; i < populationSize; i++)
@@ -89,29 +120,41 @@ public class CurveFittingGA {
 
     public void tournamentSelection() {
         for (int i = 0; i < populationSize - eliteCount; i++) {
+        	
+        	// Select from population
             int contender1 = eliteCount + (int) (Math.random() * (populationSize - eliteCount));
             int contender2 = eliteCount + (int) (Math.random() * (populationSize - eliteCount));
 
             int selectedContender;
+            
+            // Contender with best fitness wins 
+            
             if (population[contender1].fitness < population[contender2].fitness) {
                 selectedContender = contender1;
             } else {
                 selectedContender = contender2;
             }
-
+            
+            // Copy the winning contender to parents array
             parents[i].coefficients = Arrays.copyOf(population[selectedContender].coefficients, population[selectedContender].coefficients.length);
         }
     }
     
     public void nonUniformMutation() {
+    	
+    	// To perform mutation on each gene 
+    	
         for (int i = 0; i < populationSize - eliteCount; i++) {
             for (int j = 0; j <= polynomialDegree; j++) {
+            	
+            	// Do mutation if random value is less than or qual to pm
+            	
                 if (Math.random() <= Pm) {
                     // Calculate the distance from the current value to the lower and upper bounds
                     double distanceToLowerBound = children[i].coefficients[j] - lowerBound;
                     double distanceToUpperBound = upperBound - children[i].coefficients[j];
 
-                    // Determine the direction of mutation (towards the lower or upper bound)
+                    // n3rf e7na lower wala upper bound
                     boolean mutateTowardsLowerBound = Math.random() <= 0.5;
                     double distanceToMutate;
 
@@ -125,7 +168,6 @@ public class CurveFittingGA {
                     double delta = distanceToMutate * (1 - Math.pow(Math.random(), 1 - Math.pow(((double) testcase / (double) generations), b)));
                     delta = Math.round(100 * delta) / 100.0;
 
-                    // Apply the delta to the current value
                     if (mutateTowardsLowerBound) {
                         children[i].coefficients[j] -= delta;
                     } else {
@@ -140,54 +182,56 @@ public class CurveFittingGA {
     public void twoPointCrossover() {
         for (int i = 0; i < populationSize - eliteCount; i += 2) {
             double doCrossover = Math.random();
-
+            
+            // Check lw hn3ml crossover wla la 7sab el pc
+            
             if (doCrossover <= Pc) {
+            	// random 2 points
                 int point1 = (int) (Math.random() * (polynomialDegree + 1));
                 int point2 = (int) (Math.random() * (polynomialDegree + 1));
 
+                //point boundaries check
                 while (point1 == point2 || (point1 == 0 && point2 == polynomialDegree)) {
                     point1 = (int) (Math.random() * (polynomialDegree + 1));
                     point2 = (int) (Math.random() * (polynomialDegree + 1));
                 }
-
+                
+                //awl point lazm tb2a as8r mn eltanya
                 if (point1 > point2) {
                     int temp = point1;
                     point1 = point2;
                     point2 = temp;
                 }
-
-                // Perform two-point crossover
+                // First child
                 crossover(parents[i].coefficients, parents[i + 1].coefficients, children[i].coefficients, point1, point2);
+                // Second child
                 crossover(parents[i + 1].coefficients, parents[i].coefficients, children[i + 1].coefficients, point1, point2);
             } else {
+            	// lw no crossover el parents hyfdlo zy ma homa
                 children[i].coefficients = Arrays.copyOf(parents[i].coefficients, parents[i].coefficients.length);
                 children[i + 1].coefficients = Arrays.copyOf(parents[i + 1].coefficients, parents[i + 1].coefficients.length);
             }
         }
     }
 
-    private void crossover(double[] parent1, double[] parent2, double[] offspring, int point1, int point2) {
-        System.arraycopy(parent1, 0, offspring, 0, point1);
-        System.arraycopy(parent2, point1, offspring, point1, point2 - point1);
-        System.arraycopy(parent1, point2, offspring, point2, parent1.length - point2);
+    private void crossover(double[] parent1, double[] parent2, double[] child, int point1, int point2) {
+    	
+    	// copy ll offspring from parent1 from 0 to point1
+        System.arraycopy(parent1, 0, child, 0, point1);
+        
+    	// copy ll offspring from parent2 from point1 to point2
+        System.arraycopy(parent2, point1, child, point1, point2 - point1);
+        
+    	// copy ll offspring from parent1 from point2 to end
+        System.arraycopy(parent1, point2, child, point2, parent1.length - point2);
     }
 
     public void elitistReplacement() {
+    	// copy best children to replace least fit in population while preserving the elites (loop btbda2 mn elite count)
         for (int i = eliteCount; i < populationSize; i++) {
             population[i].coefficients = children[i - eliteCount].coefficients.clone();
             population[i].fitness = children[i - eliteCount].fitness;
         }
     }
 
-    public void writeToFile() {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("results.txt", true))) {
-            writer.println("Dataset: " + testcase);
-            for (int i = 0; i <= polynomialDegree; i++) {
-                writer.println("Coefficient " + i + " = " + population[0].coefficients[i]);
-            }
-            writer.println("MSE = " + population[0].fitness);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
